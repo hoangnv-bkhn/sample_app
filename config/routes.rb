02053d6d2e678af6fbcd9667661f17db22045a89
hoangnv <viethoang.ma1009@gmail.com>
2022-08-15 21:19:10 -0700

    Chapter 8

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,10 @@ Rails.application.routes.draw do
     get "/about", to: "static_pages#about"
     get "/contact", to: "static_pages#contact"
     get "/signup", to: "users#new"
-    # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+    get "/login", to: "sessions#new"
+    post "/login", to: "sessions#create"
+    delete "/logout", to: "sessions#destroy"
+
     resources :users, only: %i(new create show)
   end
 end
